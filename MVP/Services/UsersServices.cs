df76e197ce7bf2a85b92b972e499a3d017555e3d
Jeremie <jeremieruvu@gmail.com>
2020-08-25 09:16:45 -0700

    Send To Tylor

---
+++ MVP/Services/UsersServices.cs
@@ -0,0 +1,45 @@
+﻿using MVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVP.Services
+{
+  public class UsersServices : IservicesUsers<Users>
+  {
+
+    readonly DbContext dbContext;
+    public UsersServices(DbContext context)
+    {
+      dbContext = context;
+    }
+
+    public IEnumerable<Users> GetAll()
+    {
+      throw new NotImplementedException();
+    }
+      public void Add(Users entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Change(Users dbEntity, Users entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(Users entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Users Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+ 
+  }
+}
